7ff43bef894a5a88494bf3f16fed0826b6471400
Steven <stevenbellamy0@gmail.com>
2020-12-15 14:09:34 +1300

    Console log

--- resources/js/components/FormField.vue
+++ resources/js/components/FormField.vue
@@ -194,7 +194,7 @@ export default {
 
         move(evt) {
           let index = evt.newIndex;
-          console.log(newIndex);
+          console.log(evt);
           this.order.splice(index - 1, 0, this.order.splice(index, 1)[0]);
         },
 
